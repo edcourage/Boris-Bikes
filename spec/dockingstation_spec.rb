50b1d1030555b1bb2828065394d0a891000d9599
Ed Courage <edcourage@live.com>
2019-10-16 15:59:50 +0100

    chapter10 - complete

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -8,4 +8,12 @@ describe DockingStation do
   dockingstation = DockingStation.new
   expect(dockingstation).to respond_to(:release_bike)
   end
+
+  it "test class method 'release_bike' creates new instance of Bike class if bike in wokrking" do
+    dockingstation = DockingStation.new
+    bike = dockingstation.release_bike
+    expect(bike).to be_an_instance_of(Bike)
+    expect(bike).to be_working
+  end
+
 end
