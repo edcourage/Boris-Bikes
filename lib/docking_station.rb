cd11f6bc60511cf0ee67f125b8e1c1953a1a82d4
Ed Courage <edcourage@live.com>
2019-10-16 17:56:35 +0100

    chapter11 - done

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,9 +2,21 @@ require_relative 'bike'
 
 
 class DockingStation
+  attr_reader :stored_bikes
+
+  def initialize
+    @stored_bikes = []
+
+  end
+
+
 
   def release_bike
     Bike.new
+  end
+
+  def dock_bike(bike)
+    @stored_bikes << bike
   end
 
 end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -4,7 +4,7 @@
 require 'docking_station'
 
 describe DockingStation do
-  it 'checks instances of DockingStation class repond to release_bike method' do
+  it 'checks instances of DockingStation class respond to release_bike method' do
   dockingstation = DockingStation.new
   expect(dockingstation).to respond_to(:release_bike)
   end
@@ -14,6 +14,30 @@ describe DockingStation do
     bike = dockingstation.release_bike
     expect(bike).to be_an_instance_of(Bike)
     expect(bike).to be_working
+  end
+
+  it "checks instances of DockingStation class respond to dock_bike method" do
+    dockingstation = DockingStation.new
+    expect(dockingstation).to respond_to(:dock_bike)
+  end
+
+
+  it "checks that dock_bike accepts argument" do
+    dockingstation = DockingStation.new
+    bike = dockingstation.release_bike
+    expect {dockingstation.dock_bike(bike)}.not_to raise_error
+  end
+
+  it "checks the instant variable '@stored_bikes' is an array in the DockingStation class" do
+      dockingstation = DockingStation.new
+      expect(dockingstation.stored_bikes).to be_kind_of(Array)
+  end
+
+  it "checks bike we dock is added to stored_bikes array" do
+    dockingstation = DockingStation.new
+    bike = dockingstation.release_bike
+    dockingstation.dock_bike(bike)
+    expect(dockingstation.stored_bikes).to include(bike)
   end
 
 end
